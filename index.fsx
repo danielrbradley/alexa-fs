b18b236a902e41e103ea21b328b9db77868d9d27
Daniel Bradley <daniel@danielbradley.net>
2017-03-27 21:07:38 +0100

    Move existing code into Interop module, remove generics
    - Remove generics from interop layer, and just handle in wrapped models.
    - Create async wrapper to hide raw callbacks.

--- index.fsx
+++ index.fsx
@@ -1,162 +1,181 @@
 #r "node_modules/fable-core/Fable.Core.dll"
 
 open Fable.Core
+open Fable.Core.JsInterop
 
-[<Pojo>]
-type User =
-  {
-    userId : string
-    accessToken : string option
-  }
+module Interop =
+  [<Pojo>]
+  type User =
+    {
+      userId : string
+      accessToken : string option
+    }
 
-[<Pojo>]
-type Application =
-  {
-    applicationId : string
-  }
+  [<Pojo>]
+  type Application =
+    {
+      applicationId : string
+    }
 
-[<Pojo>]
-type Session<'Attributes> =
-  {
-    ``new`` : bool
-    sessionId : string
-    attributes : 'Attributes option
-    application : Application
-    user : User
-  }
+  [<Pojo>]
+  type Session =
+    {
+      ``new`` : bool
+      sessionId : string
+      attributes : obj
+      application : Application
+      user : User
+    }
 
-[<StringEnum>]
-type RequestType =
-  | [<CompiledName("LaunchRequest")>] LaunchRequest
-  | [<CompiledName("IntentRequest")>] IntentRequest
-  | [<CompiledName("SessionEndedRequest")>] SessionEndedRequest
+  [<StringEnum>]
+  type RequestType =
+    | [<CompiledName("LaunchRequest")>] LaunchRequest
+    | [<CompiledName("IntentRequest")>] IntentRequest
+    | [<CompiledName("SessionEndedRequest")>] SessionEndedRequest
 
-[<StringEnum>]
-type SessionEndedReason =
-  | [<CompiledName("USER_INITIATED")>] UserInitiated
-  | [<CompiledName("ERROR")>] Error
-  | [<CompiledName("EXCEEDED_MAX_REPROMPTS")>] ExceededMaxReprompts
+  [<StringEnum>]
+  type SessionEndedReason =
+    | [<CompiledName("USER_INITIATED")>] UserInitiated
+    | [<CompiledName("ERROR")>] Error
+    | [<CompiledName("EXCEEDED_MAX_REPROMPTS")>] ExceededMaxReprompts
 
-[<Pojo>]
-type Slot =
-  {
-    name : string
-    value : obj
-  }
+  [<Pojo>]
+  type Slot =
+    {
+      name : string
+      value : obj
+    }
 
-type Slots =
-  [<Emit("$0[$1]{{=$2}}")>]
-  abstract Item: string -> Slot with get
+  type Slots =
+    [<Emit("$0[$1]{{=$2}}")>]
+    abstract Item: string -> Slot with get
 
-[<Pojo>]
-type Intent =
-  {
-    name : string
-    slots : Slots
-  }
+  [<Pojo>]
+  type Intent =
+    {
+      name : string
+      slots : Slots
+    }
 
-[<Pojo>]
-type RequestBody =
-  {
-    requestId : string
-    timeStamp : string
-    ``type`` : RequestType
-    reason : SessionEndedReason option
-    intent : Intent option
-  }
+  [<Pojo>]
+  type RequestBody =
+    {
+      requestId : string
+      timeStamp : string
+      ``type`` : RequestType
+      reason : SessionEndedReason option
+      intent : Intent option
+    }
 
-[<Pojo>]
-type Request<'Attributes> =
-  {
-    version : string
-    session : Session<'Attributes>
-    request : RequestBody
-  }
+  [<Pojo>]
+  type Request =
+    {
+      version : string
+      session : Session
+      request : RequestBody
+    }
 
-[<StringEnum>]
-type SpeechType =
-  | [<CompiledName("PlainText")>] PlainText
-  | [<CompiledName("SSML")>] SSML
+  [<StringEnum>]
+  type SpeechType =
+    | [<CompiledName("PlainText")>] PlainText
+    | [<CompiledName("SSML")>] SSML
 
-type Speech =
-  {
-    ``type`` : SpeechType
-    text : string option
-    ssml : string option
-  }
+  type Speech =
+    {
+      ``type`` : SpeechType
+      text : string option
+      ssml : string option
+    }
 
-type Reprompt =
-  {
-    outputSpeech : Speech
-  }
+  type Reprompt =
+    {
+      outputSpeech : Speech
+    }
 
-[<StringEnum>]
-type CardType =
-  | [<CompiledName("Simple")>] Simple
-  | [<CompiledName("Standard")>] Standard
-  | [<CompiledName("LinkAccount")>] LinkAccount
+  [<StringEnum>]
+  type CardType =
+    | [<CompiledName("Simple")>] Simple
+    | [<CompiledName("Standard")>] Standard
+    | [<CompiledName("LinkAccount")>] LinkAccount
 
-type Image =
-  {
-    smallImageUrl : string
-    largeImageUrl : string
-  }
+  type Image =
+    {
+      smallImageUrl : string
+      largeImageUrl : string
+    }
 
-type Card =
-  {
-    ``type`` : CardType
-    title : string option
-    content : string option
-    text : string option
-    image : Image option
-  }
+  type Card =
+    {
+      ``type`` : CardType
+      title : string option
+      content : string option
+      text : string option
+      image : Image option
+    }
 
-type ResponseBody =
-  {
-    shouldEndSession : bool
-    outputSpeech : Speech option
-    reprompt : Reprompt option
-    card : Card option
-  }
+  type ResponseBody =
+    {
+      shouldEndSession : bool
+      outputSpeech : Speech option
+      reprompt : Reprompt option
+      card : Card option
+    }
 
-type Response<'Attributes> =
-  {
-    version : string
-    sessionAttributes : 'Attributes option
-    response : ResponseBody
-  }
+  type Response =
+    {
+      version : string
+      sessionAttributes : obj
+      response : ResponseBody
+    }
 
-[<Pojo>]
-type LambdaContext =
-  {
-    callbackWaitsForEmptyEventLoop : bool
-    logGroupName : string
-    logStreamName : string
-    functionName : string
-    memoryLimitInMB : string
-    functionVersion : string
-    invokeid : string
-    awsRequestId : string
-  }
+  [<Pojo>]
+  type LambdaContext =
+    {
+      callbackWaitsForEmptyEventLoop : bool
+      logGroupName : string
+      logStreamName : string
+      functionName : string
+      memoryLimitInMB : string
+      functionVersion : string
+      invokeid : string
+      awsRequestId : string
+    }
 
-type LambdaHandler<'A> = (Request<'A> * LambdaContext * (System.Func<exn option, Response<'A> option, unit>)) -> unit
+  type LambdaHandler = (Request * LambdaContext * (System.Func<exn option, Response option, unit>)) -> unit
 
-let handler : LambdaHandler<_> =
-  fun (request, context, callback) ->
-    let response =
-      {
-        version = "1.0.0"
-        sessionAttributes = None
-        response =
-          {
-            shouldEndSession = true
-            outputSpeech = (Some {
-              ``type`` = PlainText
-              text = Some "Hello world"
-              ssml = None
-            })
-            reprompt = None
-            card = None
-          }
-      }
-    callback.Invoke(None, Some response)
+  type AsyncLambdaHandler = LambdaContext -> Request -> Async<Response>
+
+  let createHandler (handler : AsyncLambdaHandler) : LambdaHandler =
+    fun (request, context, callback) ->
+      async {
+        try
+          let! response = handler context request
+          if request.request.``type`` = RequestType.SessionEndedRequest then
+            callback.Invoke(None, None)
+          else
+            callback.Invoke(None, Some response)
+        with ex ->
+          callback.Invoke(Some ex, None)
+      } |> Async.Start
+
+open Interop
+
+let handler = createHandler (fun context request -> async {
+  let response =
+    {
+      version = "1.0.0"
+      sessionAttributes = None
+      response =
+        {
+          shouldEndSession = true
+          outputSpeech = (Some {
+            ``type`` = PlainText
+            text = Some "Hello world"
+            ssml = None
+          })
+          reprompt = None
+          card = None
+        }
+    }
+  return response
+})
