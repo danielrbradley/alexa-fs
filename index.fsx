1f088f4b61523196b215876b4fb9ec1e3d41bde0
Daniel Bradley <daniel@danielbradley.net>
2017-03-29 22:38:33 +0100

    Add an extra example response

--- index.fsx
+++ index.fsx
@@ -344,24 +344,43 @@ module Lambda =
     })
 
 let handler = Lambda.ofHandler (None, fun request session -> async {
-  let response =
+  let responseUsingBuilder =
     Response.say (Text "Hello world!")
-    |> Response.withCard (
-      Custom
-        {
-          Title = "My Card"
-          Content = "Info about this skill"
-          Image = Some
-            {
-              SmallUrl = "https://placehold.it/720x480"
-              LargeUrl = "https://placehold.it/1200x800"
-            }
-        }
-    )
     |> Response.withReprompt (SSML """<speech>Use SSML to control how a word is <w role="ivona:VBD">read</w> out.</speech>""")
+    |> Response.withCustomCard
+      {
+        Title = "My Card"
+        Content = "Info about this skill"
+        Image = Some
+          {
+            SmallUrl = "https://placehold.it/720x480"
+            LargeUrl = "https://placehold.it/1200x800"
+          }
+      }
     |> Response.endSession
 
+  let responseUsingDomainModel =
+    {
+      Speech = Some <| Text "Hello world!"
+      Reprompt = Some <| SSML """<speech>Use SSML to control how a word is <w role="ivona:VBD">read</w> out.</speech>"""
+      Card =
+        Some (
+          Custom
+            {
+              Title = "My Card"
+              Content = "Info about this skill"
+              Image = Some
+                {
+                  SmallUrl = "https://placehold.it/720x480"
+                  LargeUrl = "https://placehold.it/1200x800"
+                }
+            }
+        )
+      EndSession = false
+    }
+
   // Prompt user to log in
-  let linkAccount = Response.linkAccount (Some (Text "Please log in."))
-  return response, None
+  let responseToLinkAccount = Response.linkAccount (Some (Text "Please log in."))
+
+  return responseUsingBuilder, None
 })
